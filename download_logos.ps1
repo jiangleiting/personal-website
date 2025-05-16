1012d9d52dbc20b2d3553437f11e234184375734
jiangleiting <jiangleiting1@gmail.com>
2025-05-16 15:00:51 +0800

    优化logo样式和显示效果：1. 添加所有网站logo 2. 优化logo背景和边框 3. 添加暗色主题支持

--- download_logos.ps1
+++ download_logos.ps1
@@ -4,21 +4,26 @@ if (-not (Test-Path $toolsDir)) {
     New-Item -ItemType Directory -Path $toolsDir -Force
 }
 
-# Logo URLs
+# Logo URLs - 使用备用图标
 $logos = @{
-    "midjourney" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/midjourney.svg"
-    "stable-diffusion" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/stablediffusion.svg"
+    "midjourney" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/discord.svg"
+    "stable-diffusion" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/pytorch.svg"
     "canva" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/canva.svg"
-    "adobe-firefly" = "https://www.adobe.com/content/dam/cc/icons/firefly.svg"
-    "unsplash" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/unsplash.svg"
-    "pexels" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/pexels.svg"
-    "pixabay" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/pixabay.svg"
-    "freepik" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/freepik.svg"
+    "adobe-firefly" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/adobe.svg"
+    "brusheezy" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/adobephotoshop.svg"
+    "dafont" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/fontawesome.svg"
+    "flaticon" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/svg.svg"
 }
 
 # 下载每个logo
 foreach ($logo in $logos.GetEnumerator()) {
     $outputPath = Join-Path $toolsDir "$($logo.Key).svg"
     Write-Host "Downloading $($logo.Key) logo to $outputPath"
-    Invoke-WebRequest -Uri $logo.Value -OutFile $outputPath
+    try {
+        Invoke-WebRequest -Uri $logo.Value -OutFile $outputPath
+        Write-Host "Successfully downloaded $($logo.Key)"
+    }
+    catch {
+        Write-Host "Failed to download $($logo.Key): $_"
+    }
 } 

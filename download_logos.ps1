5b06d0be9ad18d586fd801e5b9ad26892e2ff0ca
jiangleiting <jiangleiting1@gmail.com>
2025-05-16 15:15:12 +0800

    更新网站：1. 修正页脚年份 2. 添加所有缺失的SVG logo 3. 统一所有图标格式

--- download_logos.ps1
+++ download_logos.ps1
@@ -10,9 +10,22 @@ $logos = @{
     "stable-diffusion" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/pytorch.svg"
     "canva" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/canva.svg"
     "adobe-firefly" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/adobe.svg"
+    "unsplash" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/unsplash.svg"
+    "pexels" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/pexels.svg"
+    "pixabay" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/pixabay.svg"
+    "freepik" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/freepik.svg"
+    "mixkit" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/mix.svg"
+    "videvo" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/vimeo.svg"
+    "coverr" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/coveralls.svg"
+    "mazwai" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/mega.svg"
+    "freesound" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/soundcloud.svg"
+    "uppbeat" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/beatport.svg"
+    "soundbible" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/soundcharts.svg"
+    "bensound" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/bandcamp.svg"
     "brusheezy" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/adobephotoshop.svg"
     "dafont" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/fontawesome.svg"
     "flaticon" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/svg.svg"
+    "textures" = "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/threedotjs.svg"
 }
 
 # 下载每个logo
